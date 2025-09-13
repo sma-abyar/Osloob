832fcf1a934d76f626451ce34c60b45d064230df
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2025-09-13 19:18:33 +0330

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Osloob/',   // 👈 نام ریپوی گیت‌هابت
 })
